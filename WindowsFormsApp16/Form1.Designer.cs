cdd07755717bfa8be768a808a522c4925a8649af
yagiz <yagiz@DESKTOP-2M3AKP0>
2023-12-08 19:39:54 +0300

    Add project files.

---
+++ WindowsFormsApp16/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(32, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 62);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "BAŞLAT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(223, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "DURDUR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(223, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "SIFIRLA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 274);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "AARRGGHH";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp16/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        int zaman = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            zaman++;
+            Point konum = new Point();
+
+            if (zaman == 1)
+            {
+                konum.X = 32;
+                konum.Y = 50;
+                pictureBox1.Location = konum;
+            }
+
+            if (zaman == 2)
+            {
+                konum.X = 32;
+                konum.Y = 60;
+                pictureBox1.Location = konum;
+            }
+
+            if (zaman == 3)
+            {
+                konum.X = 32;
+                konum.Y = 70;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 4)
+            {
+                konum.X = 32;
+                konum.Y = 80;
+                pictureBox1.Location = konum; 
+            }
+            if (zaman == 5) 
+            {
+                konum.X = 32;
+                konum.Y = 90;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 6)
+            {
+                konum.X = 32;
+                konum.Y = 100;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 7)
+            {
+                konum.X = 32;
+                konum.Y = 110;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 8)
+            {
+                konum.X = 32;
+                konum.Y = 120;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 9)
+            {
+                konum.X = 32;
+                konum.Y = 130;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 10)
+            {
+                konum.X = 32;
+                konum.Y = 140;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 11)
+            {
+                konum.X = 32;
+                konum.Y = 150;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 12)
+            {
+                konum.X = 32;
+                konum.Y = 160;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 13)
+            {
+                konum.X = 32;
+                konum.Y = 170;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 14)
+            {
+                konum.X = 32;
+                konum.Y = 180;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 15)
+            {
+                konum.X = 32;
+                konum.Y = 170;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 16)
+            {
+                konum.X = 32;
+                konum.Y = 160;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 17)
+            {
+                konum.X = 32;
+                konum.Y = 150;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 18)
+            {
+                konum.X = 32;
+                konum.Y = 140;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 19)
+            {
+                konum.X = 32;
+                konum.Y = 130;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 20)
+            {
+                konum.X = 32;
+                konum.Y = 120;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 21)
+            {
+                konum.X = 32;
+                konum.Y = 110;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 22)
+            {
+                konum.X = 32;
+                konum.Y = 100;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 23)
+            {
+                konum.X = 32;
+                konum.Y = 90;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 24)
+            {
+                konum.X = 32;
+                konum.Y = 80;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 25)
+            {
+                konum.X = 32;
+                konum.Y = 70;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 26)
+            {
+                konum.X = 32;
+                konum.Y = 60;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 27)
+            {
+                konum.X = 32;
+                konum.Y = 50;
+                pictureBox1.Location = konum;
+            }
+            if (zaman == 28)
+            {
+                konum.X = 32;
+                konum.Y = 40;
+                pictureBox1.Location = konum;
+                zaman = 0;
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            zaman = 0;
+            Point konum = new Point();
+            konum.X = 32;
+            konum.Y = 40;
+            pictureBox1.Location = konum;
+        }
+    }
+}
